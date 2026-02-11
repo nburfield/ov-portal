028e050e46f60a24c4ace17a46bd12b65a6420d9
Nolan Burfield <nolan.burfield@allianceforge.net>
2026-02-10 22:30:38 -0800

    feat: add status color constants

---
+++ src/constants/statuses.js
@@ -0,0 +1,50 @@
+export const STATUS_COLORS = {
+  active: {
+    bg: 'bg-green-100 dark:bg-green-900',
+    text: 'text-green-800 dark:text-green-200',
+  },
+  inactive: {
+    bg: 'bg-gray-100 dark:bg-gray-800',
+    text: 'text-gray-600 dark:text-gray-300',
+  },
+  archived: {
+    bg: 'bg-gray-100 dark:bg-gray-800',
+    text: 'text-gray-600 dark:text-gray-300',
+  },
+  draft: {
+    bg: 'bg-blue-100 dark:bg-blue-900',
+    text: 'text-blue-800 dark:text-blue-200',
+  },
+  paused: {
+    bg: 'bg-yellow-100 dark:bg-yellow-900',
+    text: 'text-yellow-800 dark:text-yellow-200',
+  },
+  cancelled: {
+    bg: 'bg-red-100 dark:bg-red-900',
+    text: 'text-red-800 dark:text-red-200',
+  },
+  completed: {
+    bg: 'bg-green-100 dark:bg-green-900',
+    text: 'text-green-800 dark:text-green-200',
+  },
+  missed: {
+    bg: 'bg-orange-100 dark:bg-orange-900',
+    text: 'text-orange-800 dark:text-orange-200',
+  },
+  finalized: {
+    bg: 'bg-purple-100 dark:bg-purple-900',
+    text: 'text-purple-800 dark:text-purple-200',
+  },
+  paid: {
+    bg: 'bg-green-100 dark:bg-green-900',
+    text: 'text-green-800 dark:text-green-200',
+  },
+  void: {
+    bg: 'bg-red-100 dark:bg-red-900',
+    text: 'text-red-800 dark:text-red-200',
+  },
+  out_of_service: {
+    bg: 'bg-red-100 dark:bg-red-900',
+    text: 'text-red-800 dark:text-red-200',
+  },
+}
